6d0a78d4c5172fb30ba35c5bdea8b4ad4c8ecf31
janelldizon4-debug <janelldizon4@gmail.com>
2026-01-09 21:24:06 +0800

    Create member.js

---
+++ member.js
@@ -0,0 +1,7 @@
+export const members = [
+  { name: "cris",  cpm: "cris™" },
+  { name: "NOOB", cpm: "NOOB23" },
+  { name: "ENZO", cpm: "018377228" }
+];
+
+export const TOTAL_MEMBERS = members.length;
